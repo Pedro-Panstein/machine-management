a7a78da701515ae5f7be425cffb658e1e7982840
Pedro-Panstein <phfarof@gmail.com>
2024-11-01 21:36:47 -0300

    SetUp do frontend feito

---
+++ Frontend/frontend/src/Pages/LoginPage.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <>
+      <h1>Login page work's!!!</h1>
+    </>
+  );
+}
---
+++ Frontend/frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage/>
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
+
+reportWebVitals();
